f844fcf3ec709fd4c7f90733edd547e4ad170302
smnccc <ralphgianguevarra2005@gmail.com>
2026-04-30 16:54:08 +0800

    Update WebConfig.java

--- src/main/java/com/example/furreal/config/WebConfig.java
+++ src/main/java/com/example/furreal/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
